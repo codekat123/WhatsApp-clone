adcdfcdecb9cd15fbbe485d63a89f8d81b142aac
Ahmd Gaber <ahmedgaber4371@gmail.com>
2025-11-29 17:15:21 +0200

    Refactor Redis client initialization

--- users/utils.py
+++ users/utils.py
@@ -5,10 +5,9 @@ import uuid
 import redis
 from django.conf import settings
 
-redis_client = redis.StrictRedis(
-    host=getattr(settings, "REDIS_HOST", "localhost"),
-    port=getattr(settings, "REDIS_PORT", 6379),
-    db=getattr(settings, "REDIS_DB", 1),
+
+redis_client = redis.from_url(
+    getattr(settings, "REDIS_URL", "redis://localhost:6379/1"),
     decode_responses=True,
 )
 
